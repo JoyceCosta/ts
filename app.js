1222f80e1d0e9781cf5b34b3cc2909745452b42a
Joyce Costa <joycecosta90@gmail.com>
2020-12-15 14:07:38 -0300

    core types

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('code here!');
+function add(n1, n2, showResult, phrase) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //   throw new Error('input incorreto');
+    // }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 10;
+var printResult = true;
+var resultPhrase = 'Resultado: ';
+add(number1, number2, printResult, resultPhrase);
